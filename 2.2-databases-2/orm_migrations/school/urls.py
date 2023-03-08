86dfc2bac41f660880d3c67af0bc42f4ca87f3e5
kostapa <kostapa@ya.ru>
2023-03-08 22:35:35 +0300

    my_hm_orm_migrations

--- 2.2-databases-2/orm_migrations/school/urls.py
+++ 2.2-databases-2/orm_migrations/school/urls.py
@@ -1,7 +1,10 @@
+import debug_toolbar
 from django.urls import path
 
 from school.views import students_list
+from django.urls import include, path
 
 urlpatterns = [
     path('', students_list, name='students'),
+    path('__debug__/', include(debug_toolbar.urls)),
 ]
--- 2.2-databases-2/orm_migrations/website/settings.py
+++ 2.2-databases-2/orm_migrations/website/settings.py
@@ -36,10 +36,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'debug_toolbar',
     'school',
 ]
 
 MIDDLEWARE = [
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -47,6 +49,10 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+INTERNAL_IPS = [
+    '127.0.0.1',
 ]
 
 ROOT_URLCONF = 'website.urls'
@@ -76,6 +82,8 @@ DATABASES = {
         'NAME': 'netology_orm_migrations',
         'HOST': '127.0.0.1',
         'PORT': '5432',
+        'USER': 'postgres',
+        'PASSWORD': 'dialog',
     }
 }
 
