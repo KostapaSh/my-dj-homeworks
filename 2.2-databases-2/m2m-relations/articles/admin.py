4fc0190354c65ddb776c096cca7f18593bc1cc0e
kostapa <kostapa@ya.ru>
2023-03-15 20:46:51 +0300

    hw_m2m_orm2

--- 2.2-databases-2/m2m-relations/articles/admin.py
+++ 2.2-databases-2/m2m-relations/articles/admin.py
@@ -1,8 +1,39 @@
 from django.contrib import admin
+from .models import Article, ArticleTags, Tags
+from django.forms import BaseInlineFormSet
+from django.core.exceptions import ValidationError
 
-from .models import Article
 
+
+class RelationshipInlineForm(BaseInlineFormSet):
+    def clean(self):
+        is_main = 0
+        for form in self.forms:
+            print(form.cleaned_data)
+            if form.cleaned_data:
+                print(form.cleaned_data['is_main'])
+                if form.cleaned_data['is_main']==True:
+                    is_main += True
+        if is_main == 1:
+            return super().clean()
+        elif is_main == 0:
+            raise ValidationError('Укажите основной раздел')
+        else:
+            raise ValidationError('Основным может быть только один раздел')
+
+class RelationshipInline(admin.TabularInline):
+    model = ArticleTags
+    formset = RelationshipInlineForm
+    extra = 1
 
 @admin.register(Article)
-class ArticleAdmin(admin.ModelAdmin):
+class ArticlAdmin(admin.ModelAdmin):
+    inlines = [RelationshipInline]
+
+@admin.register(Tags)
+class TagsAdmin(admin.ModelAdmin):
     pass
+
+@admin.register(ArticleTags)
+class ArticleTagsAdmin(admin.ModelAdmin):
+    list_display = ['id', 'article', 'tag', 'is_main']
--- 2.2-databases-2/m2m-relations/articles/models.py
+++ 2.2-databases-2/m2m-relations/articles/models.py
@@ -9,8 +9,29 @@ class Article(models.Model):
     image = models.ImageField(null=True, blank=True, verbose_name='Изображение',)
 
     class Meta:
-        verbose_name = 'Статья'
+        verbose_name = 'Статья Article'
         verbose_name_plural = 'Статьи'
+        ordering = ['-published_at']
 
     def __str__(self):
         return self.title
+
+class Tags(models.Model):
+    name = models.CharField(max_length=30, verbose_name='Тег')
+
+    class Meta:
+        verbose_name = 'Тег Tags'
+        verbose_name_plural = 'Теги'
+
+    def __str__(self):
+        return self.name
+
+class ArticleTags(models.Model):
+    tag = models.ForeignKey(Tags, on_delete=models.CASCADE, verbose_name='Тег')
+    article = models.ForeignKey(Article, on_delete=models.CASCADE, related_name='scopes', verbose_name='Статья')
+    is_main = models.BooleanField(default=False, verbose_name='Основной')
+
+    class Meta:
+        verbose_name = 'Связи ArticleTags'
+        verbose_name_plural = 'Тег - Статья'
+        ordering = ['-is_main']
